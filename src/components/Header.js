a04f69343a54f310da12c6e216756ac9d76d8b7c
bernadette <aguirre.bernadette@outlook.com>
2022-06-12 16:49:16 -0600

    Completed Assignment

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react"; 
+
+function Header({ name }) {
+    return (
+        <>
+        <Header>
+        <h1>{name}</h1>
+        </Header>
+        </>
+    )
+}
+
+export default Header;
+
+//*Header
+//Make a Header component as a child of App. It should return:
+
+//a <header> element with the following elements inside:
+//an <h1> with the name of the blog, passed as a prop*/}
